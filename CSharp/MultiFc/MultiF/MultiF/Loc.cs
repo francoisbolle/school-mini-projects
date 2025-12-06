72626100369349a13a5eda22f2370dffd561fb7e
francoisbolle <124367425+francoisbolle@users.noreply.github.com>
2025-12-06 16:30:44 +0100

    Add files via upload

---
+++ CSharp/MultiFc/MultiF/MultiF/Loc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiF
+{
+    public class Loc
+    {
+        private string ville;
+        public bool appart;
+
+        public Loc()
+        {
+            this.appart = true;
+        }
+
+        public void set_ville(string t_ville)
+        {
+            this.ville = t_ville;
+        }
+
+        public string  get_ville()
+        {
+            return this.ville;
+        }
+    }
+}
---
+++ CSharp/MultiFc/MultiF/MultiF/Secondaire.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiF
+{
+    public partial class Secondaire : Form
+    {
+        public Secondaire(Loc t_Loc)
+        {
+            InitializeComponent();
+            this.Loc_Persist = t_Loc;
+        }
+
+        private void button_Val_Click(object sender, EventArgs e)
+        {
+            this.Loc_Persist.set_ville(this.textBox_Ville.Text);
+            this.Loc_Persist.appart = this.checkBox_Appart.Checked;
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
