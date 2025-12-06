b15d5ef28e4291f7d8c75edef87226698e40b031
francoisbolle <124367425+francoisbolle@users.noreply.github.com>
2025-12-06 16:29:04 +0100

    Add files via upload

---
+++ CSharp/MultiFa/MultiF/MultiF/Principal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiF
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+            this.inter_loc = new Loc();
+        }
+
+        private void button_Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Intro_Click(object sender, EventArgs e)
+        {
+            //this.Form_Intro = new Secondaire();   // Variable accessible sur tout le Form ==> designer
+            
+            Secondaire Form_Intro;                  // Variable locale à l'évènement
+            Form_Intro = new Secondaire(inter_loc, this.textBox_Ville, this.textBox_TypLoc);          // instanciation du Form en mémoire
+            Form_Intro.Show();
+            //this.textBox_Ville.Text = this.inter_loc.get_ville();
+            //if (this.inter_loc.appart) this.textBox_TypLoc.Text = "Appartement";
+            //else this.textBox_TypLoc.Text = "Maison";
+        }
+    }
+}
---
+++ CSharp/MultiFa/MultiF/MultiF/Secondaire.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiF
+{
+    public partial class Secondaire : Form
+    {
+        public Secondaire(Loc t_Loc, TextBox t_TbVille, TextBox t_TbTypLoc)
+        {
+            InitializeComponent();
+            //test = new Loc();
+            this.Loc_Persist = t_Loc;   // transfert des coordonnées de la cible vers le Principal
+            this.TbVille_Persist = t_TbVille;
+            this.TbTypLoc_Persist = t_TbTypLoc;
+        }
+
+        private void button_Val_Click(object sender, EventArgs e)
+        {
+            //this.test.set_ville(this.textBox_Ville.Text);
+            //this.test.appart = this.checkBox_Appart.Checked;
+            this.Loc_Persist.set_ville(this.textBox_Ville.Text);
+            this.Loc_Persist.appart = this.checkBox_Appart.Checked;
+            
+            this.TbVille_Persist.Text = this.Loc_Persist.get_ville();
+            if (this.Loc_Persist.appart) this.TbTypLoc_Persist.Text = "Appartement";
+            else this.TbTypLoc_Persist.Text = "Maison";
+
+            this.button_Cancel_Click(sender, e);
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            this.checkBox_Appart.Checked = false;
+            this.textBox_Ville.Clear();
+            this.textBox_Ville.Focus();
+        }
+
+    }
+}
